9893a6eeb59fb7f916190042a313766dd81858e3
maiiaKey <maiia.key1024@gmail.com>
2022-09-02 00:16:18 +0200

    removed price for 18cm

--- src/components/CakeCard/CakePrice.js
+++ src/components/CakeCard/CakePrice.js
@@ -6,7 +6,6 @@ function CakePrice() {
     return (
         <div>
             <ul>
-                <li key="price18"><Typography variant="body">18 cm - 45 EUR</Typography> </li>
                 <li key="price20"><Typography variant="body">20 cm - 65 EUR</Typography> </li>
                 <li key="price22"><Typography variant="body">22 cm - 75 EUR</Typography> </li>
                 <li key="price24"><Typography variant="body">24 cm - 85 EUR</Typography> </li>
